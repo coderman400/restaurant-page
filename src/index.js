9560e42c062943a78534fea5beb44bed5ba3923c
arvind <achurox1121@gmail.com>
2024-06-06 21:05:56 +0530

    add tab switching b/w home and about

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 import homepage from './homepage.js';
 import './style.css';
 import about from './about.js';
+const content = document.querySelector('#content');
+const homeBtn = document.querySelector('#home');
+const abtBtn = document.querySelector('#about');
 
-about();
+homepage();
+
+homeBtn.addEventListener("click", ()=>{
+    content.innerHTML = '';
+    homepage();
+})
+
+abtBtn.addEventListener("click", ()=>{
+    content.innerHTML = '';
+    about();
+})
+
