7a935c82ebd737612bdd15caaf39e01bd7de291e
arvind <achurox1121@gmail.com>
2024-07-03 14:35:24 +0530

    add objects for menu items, basic styling and example item

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,41 @@
 import './style.css';
+import cheeseImg from './cheese.jpg';
 export default function loadMenu(){
     const content = document.querySelector('#content')
     const div = document.createElement('div');
     div.id = 'menu-container';
 
     content.appendChild(div);
+
+    function Item(img, title, desc){
+        let imageObj = new Image();
+        imageObj.src = img;
+
+        this.img = img;
+        this.title = title;
+        this.desc=  desc;
+        
+        let itemDiv = document.createElement('div');
+        let rightDiv = document.createElement('div');
+        itemDiv.className = 'menu-item';
+        const itemPrice = document.createElement('p');
+        const itemInfo = document.createElement('p');
+    
+        itemPrice.textContent = title;
+        itemInfo.textContent = desc;
+
+        rightDiv.className= 'right-div';
+        rightDiv.appendChild(itemPrice);
+        rightDiv.appendChild(itemInfo);
+
+        itemDiv.appendChild(imageObj);
+        itemDiv.appendChild(rightDiv);
+
+        div.appendChild(itemDiv);
+
+    }
+
+    new Item(cheeseImg,'Cheese: 50eur', 'Finely crafted rat cuisine. Contains... cheese');
+
 
 }
