e6c05e9eca6e7e942c1df43e2b0372bcd6596a5b
arvind <achurox1121@gmail.com>
2024-07-10 13:45:26 +0530

    populate menu

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,9 @@
 import './style.css';
 import cheeseImg from './cheese.jpg';
+import breadCrumbs from './bread-crumbs.jpg';
+import steak from './steak.jpg';
+import rat from './rat.jpg';
+import battery from './battery.jpg';
 export default function loadMenu(){
     const content = document.querySelector('#content')
     const div = document.createElement('div');
@@ -10,7 +14,7 @@ export default function loadMenu(){
     function Item(img, title, desc){
         let imageObj = new Image();
         imageObj.src = img;
-
+        imageObj.style.width = '14rem';
         this.img = img;
         this.title = title;
         this.desc=  desc;
@@ -36,6 +40,9 @@ export default function loadMenu(){
     }
 
     new Item(cheeseImg,'Cheese: 50eur', 'Finely crafted rat cuisine. Contains... cheese');
-
-
+    new Item(breadCrumbs, 'Bread crumbs: 30eur', 'Found it on the floor. Used to be bread');
+    new Item(steak, 'Steak au Poivre with Red Wine Pan Sauce: 14eur', 'Idk abt this, kinda mid');
+    new Item(rat, 'Timothy: 1eur', "Che F. Ratt's cousin TImothy, he's a fat pig (or rat)");
+    new Item(cheeseImg,'The Same Cheese: 52eur', "It's just the cheese again, but more expensive");
+    new Item(battery, 'Double AA battery: 5eur', "Looking for a little bzzt in ur life?");
 }
